0bb84cd6ea912e4e586a084e23542ddff06845f5
Trojan <katia11489@gmail.com>
2023-06-11 21:16:41 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import '../src/css-files/index.css';
-import App from './App';
-import ReactDOM from 'react-dom/client';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<App/>
-);
